5042069300e58e94a0eb6a921eae98a01d307ae6
egillespie <erik.gillespie@gmail.com>
2014-07-14 13:24:03 -0400

    Added basic styles/layout and ability to change team names and score.

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 (function(){
-    var app = angular.module('game', []);
+    var app = angular.module('app', []);
 
-    app.createTeam = function(){
-        var name = "Team";
-        return {
-            name: name,
+    app.controller('gameController', function($scope){
+        $scope.teamOne = {
+            name: 'Team One',
             score: 0
         };
-    };
 
-    app.controller('gameController', function(){
+        $scope.teamTwo = {
+            name: 'Team Two',
+            score: 0
+        };
 
+        $scope.changeTeamOneName = function(){
+            $scope.teamOne.name = prompt('New Team Name', $scope.teamOne.name);
+        };
+
+        $scope.changeTeamTwoName = function(){
+            $scope.teamTwo.name = prompt('New Team Name', $scope.teamTwo.name);
+        };
+
+        $scope.handleKeypress = function($event){
+            switch ($event.keyCode) {
+                case 49: // '1'
+                    $scope.teamOne.score++;
+                    break;
+                case 33: // '!'
+                    $scope.teamOne.score--;
+                    break;
+                case 50: // '2'
+                    $scope.teamTwo.score++;
+                    break;
+                case 64: // '@'
+                    $scope.teamTwo.score--;
+                    break;
+            }
+        };
     });
 })();
